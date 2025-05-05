dc11068a3b5abe59aa6b5850d19d55ed0acc3dcf
Elenildo girao <elenildogirao@gmail.com>
2025-05-05 19:28:41 -0300

    atual

---
+++ Atividades Aula01/idade_condicional.py
@@ -0,0 +1,11 @@
+idade = 15
+
+if idade >= 18:
+    print("Maior de idade")
+else:
+    print("Menor de idade")
+preco = 1000 
+desconto = 15 
+
+valor_final = preco - (preco * desconto / 100) 
+print(f"\nO preço do celular com {desconto}% de desconto é R${valor_final}")
